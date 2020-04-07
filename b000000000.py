2c9c516917448aba826beb33990e258df8a25647
Marija Kostovska <marijakostovska8@outlook.com>
2020-04-07 12:08:49 +0200

    Enlever le commentaire de la ligne 8

--- b000000000.py
+++ b000000000.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: CollegeBoreal
+@author: MarijaKostovska
 """
 
 def main():
-  #print('Informatique: le rêve')
+  print('Informatique: le rêve')
+
 
 if __name__== "__main__":
   main()
