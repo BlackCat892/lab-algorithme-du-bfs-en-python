c03913cd0a71ff34ad1d22e7d19515112066d84a
Marija Kostovska <marijakostovska8@outlook.com>
2020-04-07 12:33:32 +0200

    Tutoriel complété

--- b000000000.py
+++ b000000000.py
@@ -5,20 +5,21 @@
 """
 
 eleves = {}
-eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
-eleves["Amir"]=[]
-eleves["Franck"]=[]
-eleves["Nathalie"]=[]
-eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
-eleves["Erna"]=[]
-eleves["Hassana"]=[]
-eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
-eleves["Sekou"]=[]
-eleves["Auriane"]=[]
-eleves["Corlings"]=[]
-eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
-eleves["Souleyman"]=[]
-eleves["Zack"]=[]
+eleves["Boris"] = ["Amir", "Franck", "Nathalie", "Bertrand"]
+eleves["Amir"] = []
+eleves["Franck"] = []
+eleves["Nathalie"] = []
+eleves["Bertrand"] = ["Erna", "Hassana", "Abdelkrim"]
+eleves["Erna"] = []
+eleves["Hassana"] = []
+eleves["Zoureni"] = ["Sekou", "Auriane", "Corlings"]
+eleves["Sekou"] = []
+eleves["Auriane"] = []
+eleves["Corlings"] = []
+eleves["Abdelkrim"] = ["Souleyman", "Zack", "Zoureni"]
+eleves["Souleyman"] = []
+eleves["Zack"] = []
+
 
 def personne_elue(name):
     return name == 'Zoureni'
@@ -27,9 +28,17 @@ from collections import deque
 
 
 def search(name):
+	visitees = []
   search_queue = deque()
   search_queue += eleves[name]
-  print( len(search_queue) )
+  while search_queue:
+    personne = search_queue.popleft()
+    if not personne in visitees:
+      if personne_elue(personne):
+        print(personne + " a le fameux Mac")
+        return True
+      search_queue += eleves[personne]
+      visitees.append(personne)
   return False
 
 if __name__== "__main__":
